5160acf24e57ed4de9ef4eeb21dec900d264b73a
Tuomas Heikkilä <tuomas.k.heikkila@helsinki.fi>
2024-02-05 15:41:56 +0200

    First commit

---
+++ code/network_analysis.R
@@ -0,0 +1,13 @@
+# Tuomas Heikkilä, 5 Feb, 2024
+#
+# Introduction to (social) network analysis
+
+# Clean the environment
+rm(list = ls())
+
+# Load packages
+pacman::p_load(tidyverse,
+               igraph)
+
+# Create mockup graphs for demonstration purposes
+#
